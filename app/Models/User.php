d3dab5119cc47afd4cd95352f58f1a732d4be1ff
fajarpry13 <fajarpriyo71@gmail.coom>
2026-08-28 13:58:47 +0700

    add acces panel admin filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable implements FilamentUser
      */
      public function canAccessPanel(Panel $panel): bool
     {   
-        return str_ends_with($this->email, '@democube.com');
+        return str_ends_with($this->email, '@democube.id');
     }
     protected function casts(): array
     {
