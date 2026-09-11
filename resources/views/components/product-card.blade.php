c1ad3318d78049eb95b9a14d8b3292264e40af30
fajarpry13 <fajarpriyo71@gmail.coom>
2026-09-11 16:15:03 +0700

    add hover to product-detail

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,8 +1,8 @@
-<div class="bg-white shadow-lg rounded-xl overflow-hidden ">
+<a href="{{ route('products.show', $product->slug) }}" class="bg-white shadow-lg rounded-xl overflow-hidden transition-transform hover:scale-110 duration-500">
     <img src="{{ $product->getFirstMediaUrl('thumbnail', 'medium') }}" alt="{{ $product->name }}" class="w-full aspect-square object-cover">
     <div class="p-4">
         <h3 class="font-semibold text-dark text-lg">
-            <a href="{{ route('products.show', $product->slug) }}">{{ $product->name }}</a>
+            {{ $product->name }}
         </h3>
     </div>
-</div>
+</a>
